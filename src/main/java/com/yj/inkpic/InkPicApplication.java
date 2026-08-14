e6071b901d5aadce93d6d6da758433961b2e2738
OuYJ <oyjyyds@outlook.com>
2026-08-14 11:28:04 +0800

    项目初始化

---
+++ src/main/java/com/yj/inkpic/InkPicApplication.java
@@ -0,0 +1,13 @@
+package com.yj.inkpic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkPicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InkPicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yj/inkpic/InkPicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj.inkpic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkPicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
